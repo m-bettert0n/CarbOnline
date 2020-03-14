313429213e7a44c48ad2eed4e98151d7661ae7d0
m-bettert0n <marthabetterton@hotmail.com>
2020-03-13 23:13:18 -0400

    found user history objects

--- background.js
+++ background.js
@@ -1,6 +1,24 @@
-chrome.runtime.onInstalled.addListener(function() {
+/* in the background.js set up th event based logic
+on installation of the extension
+on specific listeners
+webRequest API to listen for specific http requests?
+filters events to listen for
+*/
+chrome.runtime.onInstalled.addListener(function() { //onInstalled function. Use to initialize data maybe ?
   chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
+    chrome.history.search({
+      text: '',
+      maxResults: 0
+    }, function(historyItems){
+      if(historyItems.length === 0){
+        return 'no history'
+      }
+      else{
+        const oldestItem =  historyItems[historyItems.length-1]
+        console.log(historyItems.length)
+        return true
+      }
+    });
   });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
