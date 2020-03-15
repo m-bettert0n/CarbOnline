1c8b9ea1b7e82b791dab2a89d263d3f30f786b2e
m-bettert0n <marthabetterton@hotmail.com>
2020-03-15 12:59:52 -0400

    found the content length

--- background.js
+++ background.js
@@ -4,29 +4,14 @@ on specific listeners
 webRequest API to listen for specific http requests?
 filters events to listen for
 */
-chrome.runtime.onInstalled.addListener(function() { //onInstalled function. Use to initialize data maybe ?
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    chrome.history.search({
-      text: '',
-      maxResults: 0
-    }, function(historyItems){
-      if(historyItems.length === 0){
-        return 'no history'
-      }
-      else{
-        const oldestItem =  historyItems[historyItems.length-1]
-        console.log(historyItems.length)
-        return true
-      }
-    });
-  });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-});
+ //onInstalled function. Use to initialize data maybe ?
+
+ chrome.webRequest.onHeadersReceived.addListener(function(details){
+   const headers = details.responseHeaders;
+   const contentlength = headers.filter(header => header.name === 'content-length')
+      console.log(contentlength[0].value);
+    },
+    {
+      urls: ["<all_urls>"] ,
+      types: ["xmlhttprequest"]
+    },["responseHeaders"])
