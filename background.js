c0ce3a10e475ac112c0e60e94937acc088e8bd74
m-bettert0n <marthabetterton@hotmail.com>
2020-03-15 23:56:58 -0400

    comparison functions written

--- background.js
+++ background.js
@@ -16,23 +16,26 @@
 
  // take the totalBytes value and calculate the CO2 emmisions
  function calculateCarbon(totalBytes){
-  // first, calculate energy intensity of web data
+  // first, calculate energy intensity (in KwH) of web data
   const energy = totalBytes * (KWG_PER_GB/B_PER_GB)
+  calculateCoffee(energy)
+  calculateCar(energy)
+  calculateLight(energy)
+
   // carbon intensity of electricity used
   const totalCarbon = energy * CARBON_PER_KWG_GRID
 
   //save carbon value on the storage API
-
-  chrome.storage.sync.set({'carbonOutput': totalCarbon}, function(){
-  });
+  calculateTree(totalCarbon)
+  chrome.storage.sync.set({'carbonOutput': totalCarbon})
 }
 
 
  // calculate some of all web request data
 function sumBytes(bytes){
   totalBytes += bytes;
-  console.log(bytes)
   calculateCarbon(totalBytes);
+
 }
 
 //listens to every http request and finds the content-length property of each
@@ -51,8 +54,30 @@ function sumBytes(bytes){
       types: ["xmlhttprequest"]
     },["responseHeaders"])
 
+// function to calculate number of trees absorbing C02 per day
+function calculateTree(carbon){
+  const C_PER_TREE_DAY = 55;
+  const numOfTrees = Math.round(carbon/C_PER_TREE_DAY)
+  chrome.storage.sync.set({'trees': numOfTrees});
+}
 
+// function to show the number of hours a lightbulb would be alight for with this amount of energy
+function calculateLight(energy){
+  const NRG_LIGHT_HR = 0.06;
+  const numOfHours = Math.round(energy/NRG_LIGHT_HR)
+  chrome.storage.sync.set({'hours': numOfHours});
+}
 
+// function to show the number of miles an electric car would travel with this amount of energy
+function calculateCar(energy){
+  const KWH_PER_MILE = 3;
+  const numOfMiles = Math.round(energy/KWH_PER_MILE)
+  chrome.storage.sync.set({'miles': numOfMiles});
+}
 
-
-
+// function to show the number of coffees that could be made with this amount of energy
+function calculateCoffee(energy){
+  const NRG_PER_COFFEE = 0.0156 ;
+  const numOfCoffees = Math.round(energy/NRG_PER_COFFEE)
+  chrome.storage.sync.set({'coffee': numOfCoffees});
+}
--- popup.js
+++ popup.js
@@ -1,4 +1,8 @@
 let carbon = document.getElementById('carbon');
+let trees = document.getElementById('trees');
+let light = document.getElementById('light');
+let car = document.getElementById('car');
+let coffee = document.getElementById('coffee');
 
 chrome.storage.sync.get('carbonOutput', function(data) {
   let num = data.carbonOutput.toFixed(2);
@@ -7,3 +11,30 @@ chrome.storage.sync.get('carbonOutput', function(data) {
 });
 
 
+// button onClick produce Trees text
+chrome.storage.sync.get('trees', function(data) {
+  let num = data.trees;
+  if(num ===0) trees.innerHTML = `Not enough`
+  else trees.innerHTML = `${num} trees`
+});
+
+// button onClick produce lightbulb text
+chrome.storage.sync.get('hours', function(data) {
+  let num = data.hours;
+  if(num ===0) light.innerHTML = `Not enough energy to power a lightbulb`
+  else light.innerHTML = `${num} hours`
+});
+
+// button onClick produce electricCar text
+chrome.storage.sync.get('miles', function(data) {
+  let num = data.miles;
+  if(num ===0) car.innerHTML = `Not enough energy to travel in an electric car`
+  else car.innerHTML = `${num} miles`
+});
+
+// button onClick produce boiling water text
+chrome.storage.sync.get('coffee', function(data) {
+  let num = data.coffee;
+  if(num ===0) coffee.innerHTML = `Not enough energy to make a cup of coffee`
+  else coffee.innerHTML = `${num} cups of coffee`
+});
